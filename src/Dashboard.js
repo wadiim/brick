7c89025df9c07601f91b25ab26d5f720673e5d12
Wadim X. Janikowski <wadim.janikowski@gmail.com>
2023-12-22 10:38:31 +0100

    Extract dashboard's header to a separate component

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,44 +1,9 @@
-import './Dashboard.css';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faRightFromBracket,
-  faMagnifyingGlass
-} from '@fortawesome/free-solid-svg-icons';
-import { Link } from "react-router-dom";
+import Header from "./Header";
 
 function Dashboard() {
   return (
     <>
-      <header className="navbar navbar-expand-lg bd-navbar sticky-top bg-primary">
-        <nav className="container-xxl d-inline-flex justify-content-between">
-          <div className="navbar-brand">
-            Fupload
-          </div>
-          <form className="d-flex" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button
-              className="btn btn-secondary"
-              type="button">
-                <FontAwesomeIcon icon={faMagnifyingGlass} />
-            </button>
-          </form>
-          <div className="nav-item">
-            <Link to="/">
-              <button
-                type="button"
-                className="nav-link m-2">
-                  <FontAwesomeIcon icon={faRightFromBracket} />
-              </button>
-            </Link>
-          </div>
-        </nav>
-      </header>
+      <Header />
     </>
   );
 }
